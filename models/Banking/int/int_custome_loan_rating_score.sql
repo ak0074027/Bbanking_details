141b1e8b07aea3f97fc1ba7c6585791453041032
Abhishek Kumar <ak0074027@gmail.com>
2025-08-22 12:02:03 +0000

    .

---
+++ models/Banking/int/int_custome_loan_rating_score.sql
@@ -0,0 +1,31 @@
+{{ config(materialized='table') }}
+
+
+
+
+SELECT 
+    a.CUSTOMER_ID,
+    a.FIRST_NAME,
+    a.LAST_NAME,
+    a.DOB,
+    a.EMAIL,
+    a.PHONE,
+    a.CUSTOMER_CREATED_AT,
+
+    b.LOAN_ID,
+    b.ACCOUNT_ID,
+    b.LOAN_TYPE,
+    b.PRINCIPAL_AMOUNT,
+    b.INTEREST_RATE,
+    b.START_DATE,
+    b.END_DATE,
+
+    c.SCORE_DATE,
+    c.SCORE,
+    c.SCORE_SOURCE
+
+FROM {{ ref('wrk_customer_set') }} a
+LEFT JOIN {{ ref('wrk_loan_set') }} b
+    ON a.CUSTOMER_ID = b.CUSTOMER_ID
+LEFT JOIN {{ ref('wrk_Credit_Score_data') }} c
+    ON a.CUSTOMER_ID = c.CUSTOMER_ID
