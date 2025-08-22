0dd86006586af4193c4cc00fae102e78795410e4
Abhishek Kumar <ak0074027@gmail.com>
2025-08-22 05:57:17 +0000

    commit after new change

--- models/Banking/dw/customer_loan_data.sql
+++ models/Banking/dw/customer_loan_data.sql
@@ -1,15 +1,5 @@
 {{ config(materialized='table') }}
 
-select ld.Loan_ID,
-    ld.Loan_Type,
-    ld.Principal_Amount,
-    ld.Interest_Rate,
-    ld.Start_Date,
-    ld.End_Date,
-    ld.loan_duration_days,
-    ld.total_interest_amount,
-    cd.full_name,
-    cd.Email,
-    cd.Phone
+select *
 
-    from {{ ref('int_customer_loan_data') }} ld 
+    from {{ ref('int_customer_loan_data') }} 
