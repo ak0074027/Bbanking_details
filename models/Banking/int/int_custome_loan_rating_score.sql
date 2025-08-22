b38af3d7506a488e64bbf680d72cdc2a448b5d6a
Abhishek Kumar <ak0074027@gmail.com>
2025-08-22 12:03:22 +0000

    .

--- models/Banking/int/int_custome_loan_rating_score.sql
+++ models/Banking/int/int_custome_loan_rating_score.sql
@@ -3,7 +3,7 @@
 
 
 
-SELECT 
+With credit_score as(SELECT 
     a.CUSTOMER_ID,
     a.FIRST_NAME,
     a.LAST_NAME,
@@ -11,7 +11,6 @@ SELECT
     a.EMAIL,
     a.PHONE,
     a.CUSTOMER_CREATED_AT,
-
     b.LOAN_ID,
     b.ACCOUNT_ID,
     b.LOAN_TYPE,
@@ -19,7 +18,6 @@ SELECT
     b.INTEREST_RATE,
     b.START_DATE,
     b.END_DATE,
-
     c.SCORE_DATE,
     c.SCORE,
     c.SCORE_SOURCE
@@ -28,4 +26,6 @@ FROM {{ ref('wrk_customer_set') }} a
 LEFT JOIN {{ ref('wrk_loan_set') }} b
     ON a.CUSTOMER_ID = b.CUSTOMER_ID
 LEFT JOIN {{ ref('wrk_Credit_Score_data') }} c
-    ON a.CUSTOMER_ID = c.CUSTOMER_ID
+    ON a.CUSTOMER_ID = c.CUSTOMER_ID)
+
+    select * from credit_score
