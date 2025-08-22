0634df5b1b27ff477a2696c84785b040a152b9e2
Abhishek Kumar <ak0074027@gmail.com>
2025-08-22 12:17:19 +0000

    .

--- models/Banking/dw/custome_loan_rating_score.sql
+++ models/Banking/dw/custome_loan_rating_score.sql
@@ -1,8 +1,7 @@
-{{ config(materialized="table") }} 
+{{ config(materialized="table") }}
 
 
+with rating_score as (select * from {{ ref("int_custome_loan_rating_score") }})
 
-with rating_score as
-(select * from {{ ref("int_custome_loan_rating_score") }})
-
-select * from rating_score
+select *
+from rating_score
