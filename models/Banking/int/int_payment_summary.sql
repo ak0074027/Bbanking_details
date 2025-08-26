6da84c3e01464ee5f31b1dc4a23389681a58fe59
Abhishek Kumar <ak0074027@gmail.com>
2025-08-26 11:37:50 +0000

    .

--- models/Banking/int/int_payment_summary.sql
+++ models/Banking/int/int_payment_summary.sql
@@ -1,22 +1,23 @@
+{% raw %}
 {{ config(materialized="table") }}
 
-
-with
-    payment_data as (
-        select
-            p.payment_id,
-            p.transaction_id,
-            t.account_id,
-            a.customer_id,
-            p.card_id,
-            p.payment_mode,
-            p.payment_status,
-            p.payment_timestamp
-        from {{ ref("wrk_Payment_data") }} p
-        left join
-            {{ ref("wrk_Transaction_data") }} t on p.transaction_id = t.transaction_id
-        left join {{ ref("wrk_account_data") }} a on t.account_id = a.account_id
-    )
+with payment_data as (
+select
+p.payment_id,
+p.transaction_id,
+t.account_id,
+a.customer_id,
+p.card_id,
+p.payment_mode,
+p.payment_status,
+p.payment_timestamp
+from {{ ref("wrk_Payment_data") }} p
+left join {{ ref("wrk_Transaction_data") }} t
+on p.transaction_id = t.transaction_id
+left join {{ ref("wrk_account_data") }} a
+on t.account_id = a.account_id
+)
 
 select *
 from payment_data
+{% endraw %}
