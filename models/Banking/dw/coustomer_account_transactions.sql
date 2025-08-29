328eeea93fe89795386c045d8def554faec1f87b
Abhishek Kumar <ak0074027@gmail.com>
2025-08-29 06:56:02 +0000

    .

--- models/Banking/dw/coustomer_account_transactions.sql
+++ models/Banking/dw/coustomer_account_transactions.sql
@@ -1,29 +1,25 @@
 
 
 {{ config(materialized="table") }}
-SELECT
+
+	SELECT
     c.customer_id,
     c.first_name AS first_name,
     c.last_name AS last_name,
     c.dob,
     c.email AS email,
     c.phone,
-
-    a.account_id,
-    a.account_type,
-    a.balance,
-    a.created_at AS account_created_at,
-    
+    c.account_id,
+    c.account_type,
+    c.balance,
+    c.created_at AS account_created_at,
     c.branch_name,
     c.address AS branch_address,
-
+	a.branch_id,
     a.transaction_id,
     a.transaction_type,
     a.amount,
     a.transaction_date,
-    a.description
-
-FROM {{ ref('int_account_transaction') }} c
-LEFT JOIN {{ ref('int_customer_account_details') }} a 
-    ON c.customer_id = a.customer_id
-	and c.account_id=a.account_id
+    a.description,
+FROM {{ ref('int_customer_account_details ') }} c
+LEFT JOIN {{ ref('int_account_transaction') }} a  ON c.customer_id = a.customer_id
